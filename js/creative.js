acdbab7d036324aac1390c1b92806cacfa022f0a
alvinjtang <alvintangdesigns@gmail.com>
2019-06-23 02:07:50 -0400

    fixed link to lavieentoast in timeline and adjusted timeline point positions for phone view

--- js/creative.js
+++ js/creative.js
@@ -36,7 +36,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($('#mainNav').offset().top > 650) {
+    if ($('#mainNav').offset().top > 600) {
       $('#mainNav').addClass('navbar-shrink');
       $('#mainNav').addClass('navbar-expand-lg-hide');
       $('.nav-logo').addClass('nav-logo-hide');
